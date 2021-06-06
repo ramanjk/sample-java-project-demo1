3fb36cbf1cc3ba09b2e10c063dd59d4b5009d819
Rajan Rayappan <rajan@rathinamtrainers.com>
2021-06-06 08:04:42 +0000

    Rathinam Consultants log added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -20,6 +20,7 @@ public class App {
         Logger log = LoggerFactory.getLogger(App.class);
         log.info("Java app started");
 	log.info("Rathinam Trainers");
+	log.info("Rathinam Consultants");
     }
 
     public String getStatus() {
